d3f5f7859a0003268c39de61d1f96c687426ee7d
LucaCiancio <l.ciancio@studenti.unibg.it>
2023-09-29 13:03:26 +0200

    Update calculator.java

--- code/calcolatoreJava/calcolatore/src/main/java/esercitazioneIngSoftware/calcolatore/calculator.java
+++ code/calcolatoreJava/calcolatore/src/main/java/esercitazioneIngSoftware/calcolatore/calculator.java
@@ -1,5 +1,20 @@
 package esercitazioneIngSoftware.calcolatore;
 
-public class calculator {
+public class calculator 
+{
 
+	public int addizione(int a, int b) 
+	{
+		return  (a+b);
+	}
+	
+	public int sottrazione(int a, int b) 
+	{
+		return  (a-b);
+	}
+	
+	public int prodotto(int a, int b) 
+	{
+		return  (a*b);
+	}
 }
